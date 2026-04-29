a18f87ef8bbfc8b1ea9cd4948940503e8bc506bf
24328mw <24328mw@hvhs.school.nz>
2026-04-29 03:01:03 +0000

    Apr 29, 2026, 3:01 AM

--- t02_variables.js
+++ t02_variables.js
@@ -4,9 +4,9 @@ Name of task: variables
 console.log("Running t02_variables.js")
 // Variables
 let username = "Miriam";
-let age = "15";
-let money = "1,000,000,000";
-let year = "2026";
+let age = 15;
+let money = 1000000000;
+let year = 2026;
 
 /****************************
  main code
--- t03_maths.js
+++ t03_maths.js
@@ -5,7 +5,7 @@ console.log("Running t03_maths.js")
 // Variables
 let username = "Miriam";
 let age = 15;
-let money = 1 000 000 000;
+let money = 1000000000;
 let year = 2026;
 let birthYear
 let futureAge
@@ -20,9 +20,9 @@ console.log("You were born in "+ birthYear +".")
 console.log("In 10 years you will be "+ futureAge +" years old.")
 console.log("You have "+ money +" yen");
 money = money / 2
-console.log("You spend half of your money, now you have"+ money)
+console.log("You spend half of your money, now you have "+ money)
 money = money + 3
-console.log("Then you get ¥3, now you have"+ money)
+console.log("Then you get ¥3, now you have "+ money)
  /****************************
  functions
  ****************************/
