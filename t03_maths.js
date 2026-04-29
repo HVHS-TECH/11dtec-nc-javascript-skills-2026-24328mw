74d055a3f97edfc6f39d7bb1c32df6485547f6c8
24328mw <24328mw@hvhs.school.nz>
2026-04-29 02:58:34 +0000

    Apr 29, 2026, 2:58 AM

--- t03_maths.js
+++ t03_maths.js
@@ -4,16 +4,25 @@ Name of task: maths
 console.log("Running t03_maths.js")
 // Variables
 let username = "Miriam";
-let age = "15";
-let money = "1,000,000,000";
-let year = "2026";
-
+let age = 15;
+let money = 1 000 000 000;
+let year = 2026;
+let birthYear
+let futureAge
+birthYear = year - age
+futureAge = age + 10
 /****************************
  main code
  ****************************/
-console.log("hi "+ username +".");
-console.log("As of "+ year +" you are "+ age +" years old.);
+console.log("Hi "+ username +".");
+console.log("As of "+ year +" you are "+ age +" years old.");
+console.log("You were born in "+ birthYear +".")
+console.log("In 10 years you will be "+ futureAge +" years old.")
 console.log("You have "+ money +" yen");
+money = money / 2
+console.log("You spend half of your money, now you have"+ money)
+money = money + 3
+console.log("Then you get ¥3, now you have"+ money)
  /****************************
  functions
  ****************************/
