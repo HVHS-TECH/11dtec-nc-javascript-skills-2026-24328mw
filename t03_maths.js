6f30aaec079e69533ec663d3e2ebc0007fd4face
24328mw <24328mw@hvhs.school.nz>
2026-04-29 02:44:20 +0000

    Apr 29, 2026, 2:44 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,17 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of task: maths
+****************************/
+console.log("Running t03_maths.js")
+// Variables
+let username = "Miriam";
+let age = "15";
+let money = "1,000,000,000";
+let year = "2026";
+
+/****************************
+ main code
+ ****************************/
+console.log("hi "+ username +". As of "+ year +" you are "+ age +" years old. You have "+ money +" yen");
+ /****************************
+ functions
+ ****************************/
