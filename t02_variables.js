2765632781392c5d8ffa67cbae4bcc8d8192937f
24328mw <24328mw@hvhs.school.nz>
2026-04-29 02:26:08 +0000

    Apr 29, 2026, 2:26 AM

--- t02_variables.js
+++ t02_variables.js
@@ -3,17 +3,17 @@ Name of task: variables
 ****************************/
 console.log("Running t02_variables.js")
 // Variables
-let username = "Miriam"
-let age = "15"
-let money = "1,000,000,000"
-let year = "2026"
+let username = "Miriam";
+let age = "15";
+let money = "1,000,000,000";
+let year = "2026";
 
 /****************************
  main code
  ****************************/
-console.log("hi "+ username +".)
-console.log("As of "+ year +" you are "+ age +" years old.") 
-console.log("You have "+ money +" yen")
+console.log("hi "+ username +".);
+console.log("As of "+ year +" you are "+ age +" years old.");
+console.log("You have "+ money +" yen");
  /****************************
  functions
  ****************************/
