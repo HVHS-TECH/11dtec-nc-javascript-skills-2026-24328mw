ec80c6548b6428144eade966d8379f832db44193
24328mw <24328mw@hvhs.school.nz>
2026-04-29 02:47:08 +0000

    Apr 29, 2026, 2:47 AM

--- t03_maths.js
+++ t03_maths.js
@@ -11,7 +11,9 @@ let year = "2026";
 /****************************
  main code
  ****************************/
-console.log("hi "+ username +". As of "+ year +" you are "+ age +" years old. You have "+ money +" yen");
+console.log("hi "+ username +".");
+console.log("As of "+ year +" you are "+ age +" years old.);
+console.log("You have "+ money +" yen");
  /****************************
  functions
  ****************************/
