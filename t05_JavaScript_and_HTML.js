0a969bb7578f089a333b1e778a2ccc281d9ddff0
24328mw <24328mw@hvhs.school.nz>
2026-05-03 22:24:33 +0000

    May 3, 2026, 10:24 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,28 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Name of task: JavaScript_and_HTML.js
+****************************/
+console.log("Running t05_JavaScript_and_HTML.js")
+// Variables
+let username = "Miriam";
+let age = 15;
+let money = 1000000000;
+let year = 2026;
+let birthYear
+let futureAge
+birthYear = year - age
+futureAge = age + 10
+/****************************
+ main code
+ ****************************/
+console.log("Hi "+ username +".");
+console.log("As of "+ year +" you are "+ age +" years old.");
+console.log("You were born in "+ birthYear +".")
+console.log("In 10 years you will be "+ futureAge +" years old.")
+console.log("You have ¥"+ money);
+money = money / 2
+console.log("You spend half of your money, now you have ¥"+ money)
+money = money + 3
+console.log("Then you get ¥3, now you have ¥"+ money)
+ /****************************
+ functions
+ ****************************/
