f02c6e2da755248ba6600ad14343ed81f316c044
24328mw <24328mw@hvhs.school.nz>
2026-05-03 22:36:02 +0000

    May 3, 2026, 10:36 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -15,15 +15,15 @@ futureAge = age + 10
  main code
  ****************************/
 const OUTPUT = document.getElementById("JavaScriptOutput");
-OUTPUT.innerHTML = "<p>Hi "+ username +".</p>"
-OUTPUT.innerHTML += "<p>As of "+ year +" you are "+ age +" years old.</p>"
-OUTPUT.innerHTML += "<p>You were born in "+ birthYear +".</p>"
-OUTPUT.innerHTML += "<p>In 10 years you will be "+ futureAge +" years old.</p>"
-OUTPUT.innerHTML += "<p>You have ¥"+ money</p>"
-money = money / 2
-OUTPUT.innerHTML += "<p>You spend half of your money, now you have ¥"+ money"</p>"
-money = money + 3
-OUTPUT.innerHTML += "<p>Then you get ¥3, now you have ¥"+ money"</p>"
+OUTPUT.innerHTML = "<p>Hi "+ username +".</p>";
+OUTPUT.innerHTML += "<p>As of "+ year +" you are "+ age +" years old.</p>";
+OUTPUT.innerHTML += "<p>You were born in "+ birthYear +".</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be "+ futureAge +" years old.</p>";
+OUTPUT.innerHTML += "<p>You have ¥"+ money+ "</p>";
+money = money / 2;
+OUTPUT.innerHTML += "<p>You spend half of your money, now you have ¥"+ money + "</p>";
+money = money + 3;
+OUTPUT.innerHTML += "<p>Then you get ¥3, now you have ¥"+ money + "</p>";
  /****************************
  functions
  ****************************/
