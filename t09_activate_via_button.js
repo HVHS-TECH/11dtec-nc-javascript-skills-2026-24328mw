59c42d8a689ead1a326c45953e32b6491e7aa46f
24328mw <24328mw@hvhs.school.nz>
2026-05-07 21:31:47 +0000

    May 7, 2026, 9:31 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -1,1 +1,34 @@
-/* Paste your code from task 7 here*/
+/****************************
+Name of task: activate_via_button.js
+****************************/
+
+console.log("Running t09_activate_via_button.js")
+const OUTPUT = document.getElementById("JavaScriptOutput");
+// Variables
+let username = "Miriam";
+let age = 15;
+let money = 1000000000;
+let year = 2026;
+let birthYear
+let futureAge
+birthYear = year - age
+futureAge = age + 10
+//functions
+function welcome(){
+    OUTPUT.innerHTML = "<p>welcome to the shop</p>";
+}
+function start(){
+/****************************
+ main code
+ ****************************/
+welcome();
+OUTPUT.innerHTML += "<p>Hi "+ username +".</p>";
+OUTPUT.innerHTML += "<p>As of "+ year +" you are "+ age +" years old.</p>";
+OUTPUT.innerHTML += "<p>You were born in "+ birthYear +".</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be "+ futureAge +" years old.</p>";
+OUTPUT.innerHTML += "<p>You have ¥"+ money+ "</p>";
+money = money / 2;
+OUTPUT.innerHTML += "<p>You spend half of your money, now you have ¥"+ money + "</p>";
+money = money + 3;
+OUTPUT.innerHTML += "<p>Then you get ¥3, now you have ¥"+ money + "</p>";
+}
--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,36 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: input_from_HTML.js
+****************************/
+
+console.log("Running t10_input_from_HTML.js")
+const OUTPUT = document.getElementById("JavaScriptOutput");
+// Variables
+let age = 15;
+let money = 1000000000;
+let year = 2026;
+let birthYear
+let futureAge
+birthYear = year - age
+futureAge = age + 10
+//functions
+function welcome(){
+    OUTPUT.innerHTML = "<p>welcome to the shop</p>";
+}
+function getFormInput(){
+const NAME_FIELD = document.getElementById("nameField");
+let username = NAME_FIELD.value;
+
+/****************************
+ main code
+ ****************************/
+welcome();
+OUTPUT.innerHTML += "<p>Hi "+ username +".</p>";
+OUTPUT.innerHTML += "<p>As of "+ year +" you are "+ age +" years old.</p>";
+OUTPUT.innerHTML += "<p>You were born in "+ birthYear +".</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be "+ futureAge +" years old.</p>";
+OUTPUT.innerHTML += "<p>You have ¥"+ money+ "</p>";
+money = money / 2;
+OUTPUT.innerHTML += "<p>You spend half of your money, now you have ¥"+ money + "</p>";
+money = money + 3;
+OUTPUT.innerHTML += "<p>Then you get ¥3, now you have ¥"+ money + "</p>";
+}
