ad9355263eb7f0a07752a34c050248b558802462
24328mw <24328mw@hvhs.school.nz>
2026-05-13 03:09:53 +0000

    May 13, 2026, 3:09 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -13,6 +13,7 @@ let birthYear;
 let futureAge;
 let change;
 let chocolateOpinionArray = ["You loath chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];
+let chocolateOpinion;
 //functions
 function calculateChange(_money,_price){
     let change = _money - _price
@@ -31,20 +32,22 @@ const AGE_FIELD = document.getElementById("ageField");
 age = Number(AGE_FIELD.value);
 const MONEY_FIELD = document.getElementById("moneyField");
 money = Number(MONEY_FIELD.value);
+const CHOCOLATE_OPINION_FIELD = document.getElementById("chocolateOpinionField");
+chocolateOpinion = CHOCOLATE_OPINION_FIELD.value;
 birthYear = year - age;
 futureAge = age + 10;
 /****************************
  main code
  ****************************/
-OUTPUT.innerHTML = "<p>Hi "+ username +".</p>";
-OUTPUT.innerHTML += "<p>As of "+ year +" you are "+ age +" years old.</p>";
-OUTPUT.innerHTML += "<p>You were born in "+ birthYear +".</p>";
-OUTPUT.innerHTML += "<p>In 10 years you will be "+ futureAge +" years old.</p>";
-OUTPUT.innerHTML += "<p>You have ¥"+ money+ "</p>";
+OUTPUT.innerHTML = "<p>Hi "+username+".</p>";
+OUTPUT.innerHTML += "<p>As of "+year+" you are "+age+" years old.</p>";
+OUTPUT.innerHTML += "<p>You were born in "+birthYear+".</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be "+futureAge+" years old.</p>";
+OUTPUT.innerHTML += "<p>You have ¥"+money+"</p>";
 money = money / 2;
-OUTPUT.innerHTML += "<p>You spend half of your money, now you have ¥"+ money + "</p>";
+OUTPUT.innerHTML += "<p>You spend half of your money, now you have ¥"+money+"</p>";
 money = money + 3;
-OUTPUT.innerHTML += "<p>Then you get ¥3, now you have ¥"+ money + "</p>";
+OUTPUT.innerHTML += "<p>Then you get ¥3, now you have ¥"+money+ "</p>";
 welcome();
 displayProduct("Chocolate bar", 4);
 displayProduct("Chips", 3);
@@ -52,9 +55,10 @@ displayProduct("Drink", 2.5);
 if(money >= 4){
     OUTPUT.innerHTML += "<p>A chocolate bar costs ¥4. You CAN afford a chocolate bar.</p>"
     change = calculateChange(money, 4)
-    OUTPUT.innerHTML += "<p>You will get ¥" + change + " change</p>"
+    OUTPUT.innerHTML += "<p>You will get ¥" +change+ " change</p>"
 }
 else{
     OUTPUT.innerHTML += "<p>A chocolate bar costs ¥4. Sorry you CAN'T afford a chocolate bar.</p>"
 }
+OUTPUT.innerHTML += "<p>"+chocolateOpinionArray[chocolateOpinion]+"</p>"
 }
