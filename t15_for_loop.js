9eb6d0bc4430546521f9db6dbcde9703b2945892
24328mw <24328mw@hvhs.school.nz>
2026-05-14 22:00:03 +0000

    May 14, 2026, 10:00 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -65,13 +65,14 @@ else{
 }
 OUTPUT.innerHTML += "<p>"+chocolateOpinionArray[chocolateOpinion]+"</p>";
 for(let i = verses; i>=1; i=i){
-    if(i>=2){
+    if(i>=3){
     OUTPUT.innerHTML += "<p>" +i+ " bottles of chocolate milk on the wall</p>";
     OUTPUT.innerHTML += "<p>"+i+" bottles of chocolate milk</p>";
     OUTPUT.innerHTML += "<p>And if one of those bottles should happen to fall</p>";
     i = i - 1
     OUTPUT.innerHTML += "<p>There'd be "+i+" bottles of chocolate milk on the wall</p>";
     }
+else if(i=2)
 else{
     OUTPUT.innerHTML += "<p>" +i+ " bottle of chocolate milk on the wall</p>";
     OUTPUT.innerHTML += "<p>"+i+" bottle of chocolate milk</p>";
