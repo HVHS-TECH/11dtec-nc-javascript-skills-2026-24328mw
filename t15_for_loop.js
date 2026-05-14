1db01e2b8a8c33715609e31237f7ef9c34ff3b16
24328mw <24328mw@hvhs.school.nz>
2026-05-14 21:54:44 +0000

    May 14, 2026, 9:54 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -68,6 +68,6 @@ for(let i = verses; i>=1; i--){
     OUTPUT.innerHTML += "<p>" +i+ " bottles of chocolate milk on the wall</p>";
     OUTPUT.innerHTML += "<p>"+i+" bottles of chocolate milk</p>";
     OUTPUT.innerHTML += "<p>And if one of those bottles should happen to fall</p>";
-    OUTPUT.innerHTML += "<p>There'd be "+i - 1+" bottles of chocolate milk on the wall</p>"
+    OUTPUT.innerHTML += "<p>There'd be "+i - 1+" bottles of chocolate milk on the wall</p>";
 }
 }
