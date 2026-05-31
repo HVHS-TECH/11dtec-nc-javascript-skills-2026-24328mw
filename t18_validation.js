e81e0444f5ae28c613adc5f017a71d7570653a08
24328mw <24328mw@hvhs.school.nz>
2026-05-31 03:55:57 +0000

    May 31, 2026, 3:55 AM

--- t18_validation.js
+++ t18_validation.js
@@ -57,7 +57,10 @@ function getFormInput(){
     verses = Number(VERSES_FIELD.value);
     birthYear = year - age;
     futureAge = age + 10;
-    if (isNaN)
+    if (isNaN(username) = true){
+        OUTPUT.innerHTML = "<p>Please fill all fields correctly</p>"
+    }
+    else{
     /****************************
     main code
     ****************************/
@@ -101,6 +104,7 @@ function getFormInput(){
         else{
             OUTPUT.innerHTML += "<p>There'd be "+i+" bottle of chocolate milk on the wall</p>";
         }
+    }
     }
     }
 }
