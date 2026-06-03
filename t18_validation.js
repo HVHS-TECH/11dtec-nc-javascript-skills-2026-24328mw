4e8eb5a9ddc3728668fc1269b9ac9e8c00d484ea
24328mw <24328mw@hvhs.school.nz>
2026-06-03 02:46:57 +0000

    Jun 3, 2026, 2:46 AM

--- t18_validation.js
+++ t18_validation.js
@@ -62,6 +62,7 @@ function displayList(){
 function getFormInput(){
     const MAIN_FORM = document.getElementById("mainForm");
     testFormValidity(MAIN_FORM)
+    console.log(formValidity + "bfdsu")
     if (formValidity === false){
         OUTPUT.innerHTML = "<p>Please fill all fields correctly</p>"
     }
